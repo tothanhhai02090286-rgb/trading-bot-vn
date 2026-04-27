6039972dbba605a9ba3243c18bcf3c0d18da4067
tothanhhai02090286-rgb <tothanhhai02090286@gmail.com>
2026-04-27 08:35:27 +0700

    Update universe.py

--- universe.py
+++ universe.py
@@ -1,1 +1,17 @@
 
+UNIVERSE = [
+    "AAA","ABB","ACB","AGR","ANV","APG","APH","ASM","BAF","BCM",
+    "BFC","BID","BMP","BSR","BVB","BVS","C32","CEO","CII","CMG",
+    "CNG","CRE","CSV","CTD","CTG","CTR","CTS","DBC","DCM","DGC",
+    "DGW","DHA","DHC","DIG","DPM","DRC","DXG","EIB","ELC","EVF",
+    "FCN","FPT","FRT","FTS","GAS","GIL","GMD","GVR","HAG","HAH",
+    "HBC","HCM","HDB","HDG","HHS","HHV","HNG","HPG","HSG","IDC",
+    "IDI","IJC","ITA","IVS","KBC","KDC","KDH","KSB","LAS","LCG",
+    "LPB","MBB","MBS","MSB","MSH","MSN","MWG","NBB","NKG","NLG",
+    "NT2","NTP","OCB","OIL","ORS","PAN","PC1","PDR","PET","PHP",
+    "PLX","PNJ","POW","PVD","PVS","PVT","QNS","REE","SAB","SBT",
+    "SCR","SCS","SGT","SHB","SHS","SKG","SRC","SSB","SSI","STB",
+    "STK","SZC","TCB","TCH","TCL","TNG","TPB","TVS","VCB","VCG",
+    "VCI","VGC","VHC","VHM","VIB","VIC","VIP","VIX","VND","VNM",
+    "VOS","VPB","VPG","VRE","VSC","VTO"
+]
